a6c275a44b4dc13c01891bddf5a7379bd97409be
Eugene Wekesa <eugenco578@gmail.com>
2025-07-07 16:50:17 +0300

    `Refactor RiderHomeScreen to use StatefulWidget and add search functionality`

--- lib/screens/rider/home_screen.dart
+++ lib/screens/rider/home_screen.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-class RiderHomeScreen extends StatelessWidget {
-  const RiderHomeScreen({Key? key}) : super(key: key);
+class RiderHomeScreen extends StatefulWidget {
+  @override
+  _RiderHomeScreenState createState() => _RiderHomeScreenState();
+}
+
+class _RiderHomeScreenState extends State<RiderHomeScreen> {
+  final _pickupController = TextEditingController();
+  final _dropOffController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rider Home Screen'),
-      ),
-      body: const Center(
-        child: Text('Rider Home Screen Content'),
+      appBar: AppBar(title: Text("Find a Ride")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _pickupController,
+              decoration: InputDecoration(labelText: "Pickup Location"),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _dropOffController,
+              decoration: InputDecoration(labelText: "Drop-off Location"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text("Search"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/rider/list', arguments: {
+                  'pickup': _pickupController.text,
+                  'dropoff': _dropOffController.text,
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
